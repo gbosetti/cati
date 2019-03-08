9f0db9453dd31c256398aedf7f85f3878ec5fc5f
Lucas Ono <lucasokumura@gmail.com>
2019-03-08 15:06:04 +0100

    added alternative regex

--- images.py
+++ images.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     for cluster in data['duplicates']:
         for img in cluster:
             imgs+=1
+            # target_tweet_id  = re.search(r'(?<=/)(\d*)_(.*)\.(.*)', img, re.M | re.I)
             target_tweet_id = re.match(r'(\d*)_(.*).(.*)', img, re.M | re.I)
             res = my_connector.search({
                 "query": {
