82c7b04fc54e40b379aa1e5193e219dcbaa1b3be
gbosetti <gabybosetti@gmail.com>
2019-05-02 19:14:02 +0200

    Barchart comparing all the results of a same session

--- classification/active_learning_barchart.py
+++ classification/active_learning_barchart.py
@@ -15,16 +15,17 @@ def draw_barchart(**kwargs):
 
     data = []
 
-    for res in kwargs["results"]:
+    for loop_index in kwargs["values_by_loop"]:
 
-        max_var_value_by_config = []
-        for config in kwargs["configs"]:
-            print("...")
+        res = []
+        all_configs_at_loop = kwargs["values_by_loop"][loop_index]
+        for config_log in all_configs_at_loop:
+            res.append(config_log[kwargs["prop_name"]])
 
         trace = go.Bar(
-            x=kwargs["configs"],  # ['0-0-1', '1-0-0', '2-2-6'],
-            y=max_var_value_by_config,  # [0.9, 0.3, 0.7],
-            name=res[kwargs["trace_name"]] # at loop 10
+            x=kwargs["configs"],  # ['giraffes', 'orangutans', 'monkeys']
+            y=res,  # [0.9, 0.3, 0.7],
+            name= "at " + loop_index # at loop 10
         )
         data.append(trace)
 
@@ -34,7 +35,34 @@ def draw_barchart(**kwargs):
             xref='paper',
             x=0
         ),
-        barmode='group'
+        xaxis=dict(
+            title=kwargs["x_axis_title"],
+            tickmode='linear',
+            ticks='outside',
+            tick0=0,
+            dtick=0.25,
+            ticklen=8,
+            tickwidth=4,
+            tickangle=45,
+            tickcolor='#000'
+        ),
+        yaxis=dict(
+            title=kwargs["y_axis_title"],
+            range=[kwargs["min_y_axis_value"], 1],
+            showgrid=True,
+            showline=True,
+            tickmode='linear',
+            ticks='outside',
+            tick0=0,
+            dtick=0.01,
+            ticklen=8,
+            tickwidth=4,
+            tickcolor='#000'
+        ),
+        barmode='group',
+        autosize=False,
+        width=1200,
+        height=600
     )
     fig = go.Figure(data=data, layout=layout)
     pio.write_image(fig, kwargs["full_path"])
@@ -81,14 +109,33 @@ def print_in_file(content, path):
 
 # Initialization
 logs_folders = [f.path for f in os.scandir(logs_path) if f.is_dir() ]
-target_loops = [10, 20, 30, 60, 100]
+target_loops = [10, 20, 50, 100]
 configs = []
+values_by_loop = {}
+loop_prefix = "loop "
+for loop in target_loops:
+    values_by_loop[loop_prefix + str(loop)] = []
+
 
 def get_config_value(prop_name, full_text):
-    start_index = full_text.index("_cnf") + 5
+    start_index = full_text.index(prop_name) + 4
     end_index = start_index + 3
 
     return full_text[start_index:end_index]
+
+def get_config_name(full_text):
+
+    cnf = str(int(float(get_config_value("_cnf", full_text))*100))
+    ret = str(int(float(get_config_value("_ret", full_text))*100))
+    bgr = str(int(float(get_config_value("_bgr", full_text))*100))
+
+    return cnf + "·" + ret + "·" + bgr
+
+def get_value_at_loop(prop_name, loop_index, logs):
+
+    target_loop = [log for log in logs if log["loop"] == loop_index]
+
+    return target_loop[0]
 
 
 # Looping each session to get the HYP results
@@ -100,25 +147,30 @@ for path in logs_folders:
 
     for file in session_files:
         # Get the logs of the only file for HYP
-        logs = read_file(file.path)
+        file_content = read_file(file.path)
+        logs = [line for line in file_content if 'loop' in line]
 
-        config_name = get_config_value("_cnf", file.name)  # 'session_lyon2017_test_04_OUR_500__cnf0.0_ret0.1_bgr0.9_mda0.005_smss[500].txt'
-        config_name = get_config_value("_ret", file.name)
-        config_name = get_config_value("_bgr", file.name)
+        configs.append(get_config_name(file.name))
 
-        configs.append(file.name)
+        for loop in target_loops:
+            values_by_loop[loop_prefix + str(loop)].append(get_value_at_loop("accuracy", loop, logs))
 
 
     # Get the values from such file
-    loops_values, accuracies, wrong_answers = process_results(logs)
-    hyp_results.append({ "loops": loops_values, "_total_loops": len(loops_values),
-                         "accuracies": accuracies,
-                         "wrong_answers": wrong_answers, "_total_wrong_answers": sum(wrong_answers),
-                         "scenario_name": "Secnario " + path[-1:], "_max_accuracy": round(max(accuracies), 2)})
+    # loops_values, accuracies, wrong_answers = process_results(logs)
+    # hyp_results.append({ "loops": loops_values, "_total_loops": len(loops_values),
+    #                      "accuracies": accuracies,
+    #                      "wrong_answers": wrong_answers, "_total_wrong_answers": sum(wrong_answers),
+    #                      "scenario_name": "Secnario " + path[-1:], "_max_accuracy": round(max(accuracies), 2)})
 
-print("hyp_results:\n", json.dumps(hyp_results, indent=4, sort_keys=True))
+# print("hyp_results:\n", json.dumps(hyp_results, indent=4, sort_keys=True))
 
-draw_barchart(title="Evolution of accuracy across loops", results=hyp_results,
-                trace_name="at loop 10", full_path=os.path.join(output_path, 'OUR_accuracies' + '.png'),
-                configs=['0-0-1', '1-0-0', '2-2-6'],
-                max_var_value_by_config=[0.9, 0.3, 0.7])
+draw_barchart(title="Evolution of accuracy across loops and configurations", values_by_loop=values_by_loop,
+              x_axis_title="Configs (hw·dw·bw)", y_axis_title="Accuracy",
+              full_path=os.path.join(output_path, 'OUR_accuracies' + '.png'), configs=configs,
+              target_loops=target_loops, prop_name="accuracy", min_y_axis_value=0.93)
+
+draw_barchart(title="Evolution of the precision on positives across loops and configurations", values_by_loop=values_by_loop,
+              x_axis_title="Configs (hw·dw·bw)", y_axis_title="Precision on positives",
+              full_path=os.path.join(output_path, 'OUR_positive_precision' + '.png'), configs=configs,
+              target_loops=target_loops, prop_name="positive_precision", min_y_axis_value=0.83)
