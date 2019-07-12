3cafc68d7d669d8edb59f79894c5dccebc063f13
root <root@liris-vm-34.insa-lyon.fr>
2019-07-12 13:58:31 +0200

    merged

--- strip.rb
+++ strip.rb
@@ -6,7 +6,10 @@ end
 
 
 def filter(event)
+    logger.info("1")
     r = event.get('text').gsub(/\[do\/?[^>]*\]/,'')
+    logger.info("2")
     event.set('text',r)
+    logger.info("3")
     return [event]
 end
